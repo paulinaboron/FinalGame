6f06fe75496096f64ada589ed6cc32c96fd8a3df
paulinaboron <paulinaboron04@gmail.com>
2022-05-11 20:30:17 +0200

    first commit

---
+++ static/main.js
@@ -0,0 +1,12 @@
+const socket = io('http://localhost:3000/')
+
+socket.on('connection')
+
+const sendMessage = () =>{
+    let msg = document.getElementById("msg").value
+    socket.emit("message", msg)
+}
+
+socket.on("message", (data) =>{
+    document.getElementById("header").innerHTML = data
+})
